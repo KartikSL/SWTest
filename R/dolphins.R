dada361da9dfe6bf0842649a2be7e4181e0873a7
KartikSL <kslovekar@gmail.com>
2022-11-29 11:45:49 -0500

    Adding dataset description

---
+++ R/dolphins.R
@@ -0,0 +1,16 @@
+#' Social network of bottlenose dolphins
+#'
+#' @docType data
+#'
+#' @usage data(dolphins)
+#'
+#'
+#' @keywords datasets
+#'
+#' @references Jerome Kunegis. KONECT - The Koblenz Network Collection. In Proc. Int. Conf. on World Wide Web Companion, pages 1343-1350, 2013
+#' (\href{https://dl.acm.org/doi/10.1145/2487788.2488173}{http})
+#' @references D. Lusseau, K. Schneider, O. J. Boisseau, P. Haase, E. Slooten, and S. M. Dawson. The bottlenose dolphin community of Doubtful Sound features a large proportion of long-lasting associations. Behav. Ecol. and Sociobiol., 54:396-405, 2003.
+#'
+#' @source \href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{http://www-personal.umich.edu/~mejn/netdata/}
+#'
+"dolphins"
